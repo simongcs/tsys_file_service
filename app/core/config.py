952c411ae616e365841efd2859abf51d1d7bf886
simongcs <12417560+simongcs@users.noreply.github.com>
2024-09-23 12:27:59 +0200

    Added config

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,23 @@
 import os
+from typing import Dict
 
-from pydantic import BaseSettings
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     UPLOAD_DIRECTORY: str = os.getenv("UPLOAD_DIRECTORY")
+    AWS_S3_ENDPOINT: str = os.getenv("AWS_S3_ENDPOINT")
+    AWS_ACCESS_KEY_ID: str = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY: str = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_REGION_NAME: str = os.getenv("AWS_REGION_NAME")
+    # AWS_CONFIG: Dict[str,str] = {
+    #     "ENDPOINT_URL": "http://localstack:4566",
+    #     "AWS_ACCESS_KEY_ID": "test",
+    #     "AWS_SECRET_ACCESS_KEY": "test",
+    #     "REGION_NAME": "us-east-1",
+    # }
+    model_config = ConfigDict(env_file=".env")
 
 
 settings = Settings()
